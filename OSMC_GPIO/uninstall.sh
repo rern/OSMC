ac50aa4173fbaffbeba2d580465d670a0bafcfcc
rern <rernrern@gmail.com>
2017-02-02 16:13:24 +0700

    Update uninstall.sh

--- OSMC_GPIO/uninstall.sh
+++ OSMC_GPIO/uninstall.sh
@@ -48,13 +48,13 @@ case $answer in
 	1 ) echo;;
 	* ) echo
 		title "Uninstall packages ..."
-		[ python -c "import RPi.GPIO" > /dev/null 2>&1 ] && pip uninstall -y RPi.GPIO
-		[ dpkg -s xz-utils > /dev/null 2>&1 ] && apt remove --auto-remove -y xz-utils
-		[ dpkg -s nginx > /dev/null 2>&1 ] && apt remove --auto-remove -y nginx
-		[ dpkg -s php5-fpm > /dev/null 2>&1 ] && apt remove --auto-remove -y php5-fpm
-		[ dpkg -s gcc > /dev/null 2>&1 ] && apt remove --auto-remove -y gcc
-		[ dpkg -s python-dev > /dev/null 2>&1 ] && apt remove --auto-remove -y python-dev
-		[ dpkg -s python-pip > /dev/null 2>&1 ] && apt remove --auto-remove -y python-pip
+		python -c "import RPi.GPIO" > /dev/null 2>&1 && pip uninstall -y RPi.GPIO
+		dpkg -s xz-utils > /dev/null 2>&1 && apt remove --auto-remove -y xz-utils
+		dpkg -s nginx > /dev/null 2>&1 && apt remove --auto-remove -y nginx
+		dpkg -s php5-fpm > /dev/null 2>&1 && apt remove --auto-remove -y php5-fpm
+		dpkg -s gcc > /dev/null 2>&1 && apt remove --auto-remove -y gcc
+		dpkg -s python-dev > /dev/null 2>&1 && apt remove --auto-remove -y python-dev
+		dpkg -s python-pip > /dev/null 2>&1 && apt remove --auto-remove -y python-pip
 esac
 
 title "Remove files ..."

4297c1d08dda7d83f97fcc94060f089a95397573
rern <rernrern@gmail.com>
2017-02-23 09:02:45 +0700

    Create uninstall.sh

---
+++ aria2/uninstall.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+line2='\e[0;36m=========================================================\e[m'
+line='\e[0;36m---------------------------------------------------------\e[m'
+bar=$( echo -e "$(tput setab 6)   $(tput setab 0)" )
+info=$( echo $(tput setab 6; tput setaf 0) i $(tput setab 0; tput setaf 7) )
+
+# functions #######################################
+title2() {
+		echo -e "\n$line2\n"
+		echo -e "$bar $1"
+		echo -e "\n$line2\n"
+}
+title() {
+		echo -e "\n$line"
+		echo $1
+		echo -e "$line\n"
+}
+
+# check installed #######################################
+if ! dpkg -s aria2 > /dev/null 2>&1; then
+	title "$info Aria2 not found."
+	exit
+fi
+
+title2 "$bar Uninstall Aria2 ..."
+# uninstall package #######################################
+apt remove -y aria2 nginx
+
+# remove files #######################################
+title "Remove files ..."
+rm -v /etc/nginx/nginx.conf
+rm -v /root/.config/aria2/aria2.conf
+rm -rfv /usr/share/nginx
+
+# skip if reinstall - pwduninstall.sh re (any argument)
+[ $# -ne 0 ] && exit
+
+title2 "$bar Aria2 successfully uninstalled."
+
+rm uninstall.sh

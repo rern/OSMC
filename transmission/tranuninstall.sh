a699b2dcfefa6b5de3cd0a0724c57842d75c15ca
rern <rernrern@gmail.com>
2017-03-13 20:49:17 +0700

    Create tranuninstall.sh

---
+++ transmission/tranuninstall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+line2='\e[0;36m=========================================================\e[m'
+line='\e[0;36m---------------------------------------------------------\e[m'
+bar=$( echo -e "$(tput setab 6)   $(tput setab 0)" )
+info=$( echo $(tput setab 6; tput setaf 0) i $(tput setab 0; tput setaf 7) )
+
+# functions #######################################
+title2() {
+		echo -e "\n$line2\n"
+		echo -e "$bar $1"
+		echo -e "\n$line2\n"
+}
+title() {
+		echo -e "\n$line"
+		echo $1
+		echo -e "$line\n"
+}
+
+# check installed #######################################
+if ! dpkg -s transmission-cli > /dev/null 2>&1; then
+	title "$info Transmission not found."
+	exit
+fi
+
+title2 "$bar Uninstall Transmission ..."
+# uninstall package #######################################
+apt remove -y transmission-daemon transmission-cli
+
+# remove files #######################################
+title "Remove files ..."
+rm -rfv /etc/transmission-daemon
+
+title2 "$bar Transmission successfully uninstalled."
+
+rm tranuninstall.sh

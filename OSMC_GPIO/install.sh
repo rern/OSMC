420f5f343951ab3f43c721f42015ad154bc2b6c8
rern <rernrern@gmail.com>
2017-04-02 07:39:18 +0700

    Update install.sh

--- OSMC_GPIO/install.sh
+++ OSMC_GPIO/install.sh
@@ -25,40 +25,6 @@ rm install.sh
 
 title2 "Install $osmcgpio ..."
 
-# install OSMC GPIO #######################################
-title "Get files ..."
-
-wget -q --show-progress -O OSMC_GPIO.tar.gz "https://github.com/rern/OSMC/blob/master/OSMC_GPIO/_repo/OSMC_GPIO.tar.gz?raw=1"
-wget -q --show-progress -O uninstall.sh "https://github.com/rern/OSMC/blob/master/OSMC_GPIO/uninstall.sh?raw=1"
-chmod 755 uninstall.sh
-
-title "Install files ..."
-if [ ! -f /home/osmc/gpio.json ]; then
-	tar -xzvf OSMC_GPIO.tar.gz -C /
-else
-	tar -xzvf OSMC_GPIO.tar.gz -C / --exclude='home/osmc/gpio.json' 
-fi
-rm OSMC_GPIO.tar.gz
-
-chmod 755 /home/osmc/*.py
-chmod 666 /home/osmc/gpio.json
-chmod 755 /var/www/html/gpio/*.php
-
-chmod 644 /etc/udev/rules.d/usbsound.rules
-udevadm control --reload
-
-# add gpio home menu #######################################
-sed -i '/ActivateWindow(Settings)/ a\
-\t</shortcut>\
-\t<shortcut>\
-\t\t<defaultID />\
-\t\t<label>GPIO</label>\
-\t\t<label2>Custom item</label2>\
-\t\t<icon>DefaultShortcut.png</icon>\
-\t\t<thumb />\
-\t\t<action>ActivateWindow(1111)</action>
-' /home/osmc/.kodi/userdata/addon_data/script.skinshortcuts/mainmenu.DATA.xml
-
 # install packages #######################################
 title "$info Update package databases"
 echo -e '  \e[0;36m0\e[m Skip'
@@ -99,9 +65,43 @@ if ! python -c "import RPi.GPIO" > /dev/null 2>&1; then
 	yes | pip install RPi.GPIO
 fi
 
+# install OSMC GPIO #######################################
+title "Get files ..."
+
+wget -q --show-progress -O OSMC_GPIO.tar.gz "https://github.com/rern/OSMC/blob/master/OSMC_GPIO/_repo/OSMC_GPIO.tar.gz?raw=1"
+wget -q --show-progress -O uninstall.sh "https://github.com/rern/OSMC/blob/master/OSMC_GPIO/uninstall.sh?raw=1"
+chmod 755 uninstall.sh
+
+title "Install files ..."
+if [ ! -f /home/osmc/gpio.json ]; then
+	tar -xzvf OSMC_GPIO.tar.gz -C /
+else
+	tar -xzvf OSMC_GPIO.tar.gz -C / --exclude='home/osmc/gpio.json' 
+fi
+rm OSMC_GPIO.tar.gz
+
+chmod 755 /home/osmc/*.py
+chmod 666 /home/osmc/gpio.json
+chmod 755 /var/www/html/gpio/*.php
+
+chmod 644 /etc/udev/rules.d/usbsound.rules
+udevadm control --reload
+
 # set initial gpio #######################################
 /home/osmc/gpioset.py
 systemctl enable gpioset
+
+# add gpio home menu #######################################
+sed -i '/ActivateWindow(Settings)/ a\
+\t</shortcut>\
+\t<shortcut>\
+\t\t<defaultID />\
+\t\t<label>GPIO</label>\
+\t\t<label2>Custom item</label2>\
+\t\t<icon>DefaultShortcut.png</icon>\
+\t\t<thumb />\
+\t\t<action>ActivateWindow(1111)</action>
+' /home/osmc/.kodi/userdata/addon_data/script.skinshortcuts/mainmenu.DATA.xml
 
 title2 "$osmcgpio successfully installed."
 echo $info 'Browser: [OSMC_IP]/gpio/ for settings.'

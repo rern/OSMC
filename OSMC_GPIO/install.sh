350ef7218d5716f9b32e182e834ed8933b5b03c9
rern <rernrern@gmail.com>
2017-05-23 11:05:27 +0700

    Update install.sh

--- OSMC_GPIO/install.sh
+++ OSMC_GPIO/install.sh
@@ -97,8 +97,7 @@ systemctl restart nginx
 file='/usr/share/kodi/addons/skin.osmc/16x9/DialogButtonMenu.xml'
 
 line=$( sed -n '/Quit()/{=}' $file ) # normal
-line=$(( $line - 2 )) # 2 lines up
-sed -i -e $''"$line"' i\
+sed -i -e $''"$(( $line - 2 ))"' i\
 \t\t\t\t\t<item>\
 \t\t\t\t\t\t<label>GPIO On</label>\
 \t\t\t\t\t\t<onclick>RunScript(/home/osmc/gpioonsudo.py)</onclick>\

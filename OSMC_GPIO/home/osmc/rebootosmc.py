fdd869713a68c961f252230038b3ca449330a723
rern <rernrern@gmail.com>
2017-06-25 21:37:59 +0700

    Update rebootosmc.py

--- OSMC_GPIO/home/osmc/rebootosmc.py
+++ OSMC_GPIO/home/osmc/rebootosmc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
 
-os.system('sudo /home/osmc/gpiooffsudo.py p')
+os.system('sudo /home/osmc/gpiooffsudo.py r')
 os.system('sudo /home/osmc/rebootosmc.sh')
 
 # current raspbian has no /sys/module/bcm2709/parameters/reboot_part > use noobd.conf instead

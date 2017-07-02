2521e1734f9a903ac95c2e5f2cd906383d3162db
rern <rernrern@gmail.com>
2017-07-02 21:10:04 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-### force hdmi mode ################################################################
+line2='\e[0;36m=========================================================\e[m'
+line='\e[0;36m---------------------------------------------------------\e[m'
+bar=$( echo -e "$(tput setab 6)   $(tput setab 0)" )
+
+title2() {
+	echo -e "\n$line2\n"
+	echo -e "$bar $1"
+	echo -e "\n$line2\n"
+}
+title() {
+	echo -e "\n$line"
+	echo $1
+	echo -e "$line\n"
+}
+
+title2 "Set HDMI mode ..."
+#################################################################################
 echo '
 hdmi_group=1
 hdmi_mode=31
 ' >> /boot/config.txt
 
-### set fstab for usb drive ########################################################
+title2 "Set fstab for /mnt/hdd ..."
+#################################################################################
 mnt0=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
 label=${mnt0##/*/}
 mnt=/mnt/$label
@@ -15,12 +32,14 @@ echo "/dev/sda1       $mnt           ext4     defaults,noatime  0   0" >> /etc/f
 umount -l $mnt0
 mount -a
 
-### set apt cache to usb drive ####################################################
+title2 "Set apt cache to usb drive ..."
+#################################################################################
 rm -r /var/cache/apt
 mkdir -p $mnt/varcache/apt
 ln -s $mnt/varcache/apt /var/cache/apt
 
-### Settings ######################################################################
+title2 "Restore settings ..."
+#################################################################################
 # 'skin shortcuts' addon
 #apt update
 #apt install -y bsdtar
@@ -38,7 +57,8 @@ wget -qN --show-progress $gitpath/guisettings.xml -P /home/osmc/.kodi/userdata
 touch /walkthrough_completed
 systemctl restart mediacenter
 
-### samba ##########################################################################
+title2 "Install samba"
+#################################################################################
 apt install -y samba
 # make usb drive a common between os for smb.conf
 [[ ! -e $mnt/samba/smb.conf ]] && wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/smb.conf -P $mnt/samba
@@ -49,16 +69,21 @@ systemctl restart smbd
 # set samba password
 smbpasswd -a root
 
-### Transmission ####################################################################
+# Transmission
+#################################################################################
 wget -qN --show-progress https://github.com/rern/OSMC/raw/master/transmission/install.sh; chmod +x install.sh; ./install.sh 1
 
-### Aria2 ###########################################################################
+# Aria2
+#################################################################################
 wget -qN --show-progress https://github.com/rern/OSMC/raw/master/aria2/install.sh; chmod +x install.sh; ./install.sh 1
 
-### GPIO ############################################################################
+# GPIO
+#################################################################################
 wget -qN --show-progress https://github.com/rern/OSMC/raw/master/OSMC_GPIO/install.sh; chmod +x install.sh; ./install.sh 1
 # make usb drive a common between os for gpio.json
 [[ ! -e $mnt/gpio/gpio.json ]] && wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/gpio.json -P $mnt/gpio
 rm /home/osmc/gpio.json
 ln -s $mnt/gpio/gpio.json /home/osmc/gpio.json
 systemctl restart gpioset
+
+title "Finished."

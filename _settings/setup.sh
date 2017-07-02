9dd9db3f4872288e2b2117673ae4b59445a1af6f
rern <rernrern@gmail.com>
2017-07-02 20:46:23 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -23,12 +23,13 @@ ln -s $mnt/varcache/apt /var/cache/apt
 ### Settings ######################################################################
 # 'skin shortcuts' addon
 #apt update
-apt install -y bsdtar
+#apt install -y bsdtar
 #wget -qN --show-progress https://github.com/BigNoid/script.skinshortcuts/archive/master.zip
 #bsdtar -xf master.zip -C /home/osmc/.kodi/addons
 #mv /home/osmc/.kodi/addons/script.skinshortcuts-master /home/osmc/.kodi/addons/script.skinshortcuts
 #chown -R osmc:osmc /home/osmc/.kodi/addons/script.skinshortcuts
 #rm master.zip
+
 # customized file
 gitpath=https://github.com/rern/OSMC/raw/master/_settings
 wget -qN --show-progress $gitpath/mainmenu.DATA.xml -P /home/osmc/.kodi/userdata/addon_data/script.skinshortcuts
@@ -49,13 +50,13 @@ systemctl restart smbd
 smbpasswd -a root
 
 ### Transmission ####################################################################
-wget -qN --show-progress https://github.com/rern/OSMC/raw/master/transmission/install.sh; chmod +x install.sh; ./install.sh
+wget -qN --show-progress https://github.com/rern/OSMC/raw/master/transmission/install.sh; chmod +x install.sh; ./install.sh 1
 
 ### Aria2 ###########################################################################
-wget -qN --show-progress https://github.com/rern/OSMC/raw/master/aria2/install.sh; chmod +x install.sh; ./install.sh
+wget -qN --show-progress https://github.com/rern/OSMC/raw/master/aria2/install.sh; chmod +x install.sh; ./install.sh 1
 
 ### GPIO ############################################################################
-wget -qN --show-progress https://github.com/rern/OSMC/raw/master/OSMC_GPIO/install.sh; chmod +x install.sh; ./install.sh
+wget -qN --show-progress https://github.com/rern/OSMC/raw/master/OSMC_GPIO/install.sh; chmod +x install.sh; ./install.sh 1
 # make usb drive a common between os for gpio.json
 [[ ! -e $mnt/gpio/gpio.json ]] && wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/gpio.json -P $mnt/gpio
 rm /home/osmc/gpio.json

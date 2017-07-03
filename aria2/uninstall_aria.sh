862033899cc0794caa474ecdcf7ec1a2fb697df3
rern <rernrern@gmail.com>
2017-07-03 17:04:43 +0700

    Update uninstall_aria.sh

--- aria2/uninstall_aria.sh
+++ aria2/uninstall_aria.sh
@@ -39,7 +39,7 @@ rm -rv /var/www/html/aria2
 [ $# -ne 0 ] && exit
 
 echo 'Nginx still installed.'
-echo 'Remove: apt remove nginx'
+echo 'Remove: apt remove nginx nginx-common nginx-full'
 title2 "Aria2 successfully uninstalled."
 
 rm uninstall_aria.sh

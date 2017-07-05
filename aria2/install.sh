e96e8c1083d3c680d214a3eb597aa7eb01934f84
rern <rernrern@gmail.com>
2017-07-05 20:51:51 +0700

    Update install.sh

--- aria2/install.sh
+++ aria2/install.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
 # install.sh - run as root
-rm install.sh
 
-line2=$( printf '\e[0;36m%*s\e[m\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' = )
-line=$( printf '\e[0;36m%*s\e[m\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - )
-bar=$( echo -e "$(tput setab 6)   $(tput setab 0)" )
-info=$( echo $(tput setab 6; tput setaf 0) i $(tput setab 0; tput setaf 7) )
-
-title2() {
-	echo $line2
-	echo -e "$bar $1"
-	echo $line2
-}
-title() {
-	echo $line
-	echo -e "$1"
-	echo $line
-}
-titleend() {
-	echo -e "\n$1"
-	echo $line
-}
+# import heading function
+wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh
+chmod +x f_heading.sh
+. f_heading.sh
+rm install.sh f_heading.sh
 
 if type aria2c &>/dev/null; then
 	titleend "$info Aria2 already installed."

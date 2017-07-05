22b4cc1016ede1425c41a4b50df6a6cac3568772
rern <rernrern@gmail.com>
2017-07-05 21:26:01 +0700

    Update install.sh

--- aria2/install.sh
+++ aria2/install.sh
@@ -5,6 +5,8 @@
 # import heading function
 wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh; . f_heading.sh; rm f_heading.sh
 
+rm install.sh
+
 if type aria2c &>/dev/null; then
 	titleend "$info Aria2 already installed."
 	exit

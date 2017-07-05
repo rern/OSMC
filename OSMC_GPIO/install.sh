ac812df2cc9e2480c632b1f9895e8abd3559c94b
rern <rernrern@gmail.com>
2017-07-05 21:24:02 +0700

    Update install.sh

--- OSMC_GPIO/install.sh
+++ OSMC_GPIO/install.sh
@@ -3,10 +3,9 @@
 # install.sh - run as root
 
 # import heading function
-wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh
-chmod +x f_heading.sh
-. f_heading.sh
-rm install.sh f_heading.sh
+wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh; . f_heading.sh; rm f_heading.sh
+
+rm install.sh
 
 title2 "Install $osmcgpio ..."
 

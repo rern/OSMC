7669b08ae24f68483881e4810edfedc6dc7227a4
rern <rernrern@gmail.com>
2017-07-09 18:30:58 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -50,7 +50,7 @@ sqlite3 $dbpath/Addons27.db "UPDATE installed SET enabled = 1 WHERE addonID = 's
 sqlite3 $dbpath/Addons27.db "UPDATE installed SET enabled = 1 WHERE addonID = 'script.skinshortcuts'"
 # refresh addons data after enable
 xbmc-send -a "UpdateLocalAddons()"
-#sleep 2
+sleep 2
 # force reload skin
 #xbmc-send -a "ReloadSkin()"
 #title "Skin reloaded"

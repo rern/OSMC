22e6dbe119bbf027c408824683c651cc21009ccd
rern <rernrern@gmail.com>
2017-07-11 18:20:33 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -25,7 +25,7 @@ if (( $# == 0 )); then # with no argument
 	read -n 1 anspwd
 	[[ $anspwd == 1 ]] && setpwd
 
-	title $info Install WebUI alternative \(Transmission Web Control\):
+	title "$info Install WebUI alternative (Transmission Web Control):"
 	echo -e '  \e[0;36m0\e[m No'
 	echo -e '  \e[0;36m1\e[m Yes'
 	echo

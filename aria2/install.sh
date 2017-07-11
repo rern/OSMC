e6dc21848f4f3f1865c35ee550acd318fd73e330
rern <rernrern@gmail.com>
2017-07-11 15:54:44 +0700

    Update install.sh

--- aria2/install.sh
+++ aria2/install.sh
@@ -5,18 +5,18 @@
 #   any argument = no prompt + no package update
 
 # import heading function
-wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh; . f_heading.sh; rm f_heading.sh
+wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
 rm install.sh
 
 if type aria2c &>/dev/null; then
-	titleinfo "Aria2 already installed."
+	title $info Aria2 already installed.
 	exit
 fi
 
 if (( $# == 0 )); then
 	# user input
-	titleinfo "Start Aria2 on system startup:"
+	title $info Start Aria2 on system startup:
 	echo -e '  \e[0;36m0\e[m No'
 	echo -e '  \e[0;36m1\e[m Yes'
 	echo
@@ -30,16 +30,16 @@ fi
 wget -qN --show-progress https://github.com/rern/OSMC/raw/master/aria2/uninstall_aria.sh
 chmod +x uninstall_aria.sh
 
-title2 "Install Aria2 ..."
+title -l = $bar Install Aria2 ...
 # skip with any argument
 if (( $# == 0 )); then
-	title "Update package databases"
+	title Update package databases
 	apt update
 fi
 
 apt install -y aria2
 if ! type bsdtar &>/dev/null; then
-	title "Install bsdtar ..."
+	title Install bsdtar ...
 	apt install -y bsdtar
 fi
 
@@ -52,7 +52,7 @@ else
 	path=/root/aria2
 fi
 if (( $# == 0 )); then
-	title "Get WebUI files ..."
+	title Get WebUI files ...
 	wget -qN --show-progress https://github.com/ziahamza/webui-aria2/archive/master.zip
 	mkdir -p $path/web
 	bsdtar -xf master.zip -s'|[^/]*/||' -C $path/web
@@ -85,14 +85,15 @@ WantedBy=multi-user.target
 
 # start
 [[ $ansstartup == 1 ]] && systemctl enable aria2
-title "Start Aria2 ..."
+title Start Aria2 ...
 systemctl start aria2
 
-title2 "Aria2 installed and started successfully."
+title -l = $bar Aria2 installed and started successfully.
 echo 'Uninstall: ./uninstall_aria.sh'
+echo
 echo 'Run: sudo systemctl [ start /stop ] aria2'
 echo 'Startup: sudo systemctl [ enable /disable ] aria2'
 echo
 echo "Download directory: $path"
 echo "$info OSMC web interface changed to port:80"
-titleend "WebUI: [OSMC_IP]/aria2/"
+title -nt WebUI: [OSMC_IP]/aria2/

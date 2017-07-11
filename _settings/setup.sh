a25e2608556cf3edd305e5f5c425c9073024c387
rern <rernrern@gmail.com>
2017-07-11 17:50:46 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -3,23 +3,23 @@
 timestart=$( date +%s )
 
 # import heading and password function
-wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh; . f_heading.sh; rm f_heading.sh
+wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 wget -qN https://github.com/rern/tips/raw/master/bash/f_password.sh; . f_password.sh; rm f_password.sh
 
 rm setup.sh
 
-titlebar "This setup will take 11 min."
+title $bar This setup will take 11 min.
 echo
 
 # passwords
-titleinfo "root password for Samba and Transmission ..."
+title $info root password for Samba and Transmission ...
 setpwd
 
-titlebar "Update package database ..."
+title $bar Update package database ...
 #################################################################################
 apt update
 
-titlebar "Restore settings ..."
+title $bar Restore settings ...
 #################################################################################
 gitpath=https://github.com/rern/OSMC/raw/master/_settings
 kodipath=/home/osmc/.kodi/userdata
@@ -27,12 +27,12 @@ addonpath=/home/osmc/.kodi/addons
 pkgpath=$addonpath/packages
 dbpath=$kodipath/Database
 
-titlebar "Install bsdtar ..."
+title $bar Install bsdtar ...
 apt install -y bsdtar
 
-titlebar "Install skin.shortcuts addons ..."
+title $bar Install skin.shortcuts addons ...
 # 'skin shortcuts' addon
-title "Get addons and depends ..."
+title Get addons and depends ...
 wget -qN --show-progress https://github.com/BigNoid/script.skinshortcuts/archive/master.zip -O $pkgpath/script.skinshortcuts.zip
 wget -qN --show-progress https://github.com/XBMC-Addons/script.module.simplejson/archive/master.zip -O $pkgpath/script.module.simplejson.zip
 wget -qN --show-progress http://mirrors.kodi.tv/addons/jarvis/script.module.unidecode/script.module.unidecode-0.4.16.zip -O $pkgpath/script.module.unidecode.zip
@@ -41,7 +41,7 @@ bsdtar -xf $pkgpath/script.module.simplejson.zip -C $addonpath
 bsdtar -xf $pkgpath/script.module.unidecode.zip -C $addonpath
 chown -R osmc:osmc $addonpath
 # add addons to database
-title "Update addons database ..."
+title Update addons database ...
 xbmc-send -a "UpdateLocalAddons()"
 sleep 2
 # enable addons in database
@@ -52,7 +52,7 @@ sqlite3 $dbpath/Addons27.db "UPDATE installed SET enabled = 1 WHERE addonID = 's
 xbmc-send -a "UpdateLocalAddons()"
 sleep 2
 # force reload skin
-#xbmc-send -a "ReloadSkin()"
+#xbmc-send -a "ReloadSkin()" # !!! reset guisettings.xml
 #title "Skin reloaded"
 
 # get backup settings
@@ -61,9 +61,9 @@ wget -qN --show-progress $gitpath/mainmenu.DATA.xml -P $kodipath/addon_data/scri
 chown -R osmc:osmc $kodipath
 
 systemctl restart mediacenter
-title "OSMC restarted."
+title OSMC restarted.
 
-title2 "Install Samba ..."
+title -l = $bar Install Samba ...
 #################################################################################
 apt install -y samba
 wget -q --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/smb.conf -O /etc/samba/smb.conf
@@ -72,7 +72,7 @@ systemctl restart nmbd smbd
 # set samba password
 (echo $pwd1; echo $pwd1) | smbpasswd -s -a root
 
-title2 "Samba installed successfully." 
+title -l = $bar Samba installed successfully.
 
 # Transmission
 #################################################################################
@@ -98,5 +98,5 @@ timediff=$(( $timeend - $timestart ))
 timemin=$(( $timediff / 60 ))
 timesec=$(( $timediff % 60 ))
 
-title2 "Setup finished successfully."
-titleend "Duration: $timemin min $timesec sec"
+title -l = $bar Setup finished successfully.
+title -nt "Duration: $timemin min $timesec sec"

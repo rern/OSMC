c65f33e787497168b3bb1c9c497cc46d76badc87
rern <rernrern@gmail.com>
2017-07-12 20:59:23 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-timestart=$( date +%s )
-
 # import heading and password function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 wget -qN https://github.com/rern/tips/raw/master/bash/f_password.sh; . f_password.sh; rm f_password.sh
@@ -14,6 +12,8 @@ echo
 # passwords
 title $info root password for Samba and Transmission ...
 setpwd
+
+timestart=$( date +%s )
 
 title $bar Update package database ...
 #################################################################################

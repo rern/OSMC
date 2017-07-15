76487d5a1947d19d947790994ebfec8f2009d1e3
rern <rernrern@gmail.com>
2017-07-16 06:40:29 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -44,9 +44,9 @@ sleep 2
 sqlite3 $dbpath/Addons27.db "UPDATE installed SET enabled = 1 WHERE addonID = 'script.module.simplejson'"
 sqlite3 $dbpath/Addons27.db "UPDATE installed SET enabled = 1 WHERE addonID = 'script.module.unidecode'"
 sqlite3 $dbpath/Addons27.db "UPDATE installed SET enabled = 1 WHERE addonID = 'script.skinshortcuts'"
-# refresh addons data after enable
-xbmc-send -a "UpdateLocalAddons()"
-sleep 2
+# skin reload must refresh addons data after enable
+#xbmc-send -a "UpdateLocalAddons()"
+#sleep 2
 # force reload skin
 #xbmc-send -a "ReloadSkin()" # !!! reset guisettings.xml
 #title "Skin reloaded"

ea7806c8bf4381cc4b3db5c7c9807a0c41ead4e2
rern <rernrern@gmail.com>
2017-07-23 20:56:51 +0700

    Update install.sh

--- aria2/install.sh
+++ aria2/install.sh
@@ -4,10 +4,10 @@
 #   [startup] = 1 / null
 #   any argument = no prompt + no package update
 
+rm $0
+
 # import heading function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
-
-rm install.sh
 
 if type aria2c &>/dev/null; then
 	title "$info Aria2 already installed."

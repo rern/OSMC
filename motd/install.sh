74469cee32fa069634e57fb3fab30353ab118228
rern <rernrern@gmail.com>
2017-07-24 17:43:05 +0700

    Update install.sh

--- motd/install.sh
+++ motd/install.sh
@@ -2,15 +2,17 @@
 
 rm $0
 
+color=33
+
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
 title -l = "$bar Install OSMC logo motd ..."
 wget -qN --show-progress https://github.com/rern/OSMC/raw/master/motd/uninstall_motd.sh; chmod +x uninstall_motd.sh
 wget -qN --show-progress https://github.com/rern/OSMC/raw/master/motd/motd.banner -P /etc
+
 echo '#!/bin/bash
-color=33
-echo -e "\e[38;5;${color}m$( < /etc/motd.banner )\e[0m\n"
-PS1=$( echo -e "\u@\e[38;5;${color}m\h\e[0m:\W $" )
+echo -e "\e[38;5;'${color}'m$( < /etc/motd.banner )\e[0m\n"
+PS1='\''\u@\e[38;5;'${color}'m\h\e[0m:\W \$ '\'' # sigle quote only
 ' > /etc/profile.d/motd.sh
 
 mv /etc/motd{,.original}

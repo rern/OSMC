2f146990b472f7cb7d60e4eec2a6316a67d06567
rern <rernrern@gmail.com>
2017-07-25 16:59:10 +0700

    Update uninstall_motd.sh

--- motd/uninstall_motd.sh
+++ motd/uninstall_motd.sh
@@ -5,7 +5,7 @@ wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; r
 title -l = "$bar Uninstall OSMC logo motd ..."
 
 mv /etc/motd{.original,}
-rm /etc/motd.banner
+rm /etc/motd.logo
 rm /etc/profile.d/motd.sh
 
 title -nt "$info Relogin to see original motd."

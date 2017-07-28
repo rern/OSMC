c55bf07f3c5ce671c4e7bc4038621b34031f2d43
rern <rernrern@gmail.com>
2017-07-28 15:38:25 +0700

    Update install.sh

--- motd/install.sh
+++ motd/install.sh
@@ -20,7 +20,7 @@ echo -e "\e[38;5;${color}m$( < /etc/motd.logo )\e[0m"
 sed -i -e '/^PS1=/ s/^/#/
 ' -e '/PS1=/ a\
 color=242\
-PS1=\x27\\e[38;5;\x27$color\x27m\\u@\\h:\\e[0m\\w \\$ \x27
+PS1=\x27\\[\\e[38;5;\x27$color\x27m\\]\\u@\\h:\\[\\e[0m\\]\\w \\$ \x27
 ' /etc/bash.bashrc
 
 echo -e "\nUninstall: ./uninstall_motd.sh"

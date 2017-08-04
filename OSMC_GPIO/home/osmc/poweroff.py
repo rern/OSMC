14c1c0411a59b77e87bae5e009db8cbe75316498
rern <rernrern@gmail.com>
2017-08-05 06:54:10 +0700

    Update poweroff.py

--- OSMC_GPIO/home/osmc/poweroff.py
+++ OSMC_GPIO/home/osmc/poweroff.py
@@ -3,31 +3,8 @@
 # need root access to /dev/mem, /sys/module
 # this script cannot run sudo by itself
 import gpiooff
-import RPi.GPIO as GPIO
-import json
 import os
 import sys
-
-with open('/home/osmc/gpio.json') as jsonfile:
-	gpio = json.load(jsonfile)
-	
-on = gpio['on']
-off = gpio['off']
-
-on1 = int(on['on1'])
-on2 = int(on['on2'])
-on3 = int(on['on3'])
-on4 = int(on['on4'])
-
-onx = [on1, on2, on3, on4]
-onx = [i for i in onx if i != 0]
-
-GPIO.setwarnings(0)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(onx, GPIO.OUT)
-
-if GPIO.input(onx[1]) == 0:
-	os.system('/usr/bin/sudo /home/osmc/gpiooff.py o &')
 	
 if len(sys.argv) == 1: # no argument
 	os.system('/usr/bin/xbmc-send -a "Powerdown"')

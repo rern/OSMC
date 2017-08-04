44742ea67a1be51d5da5f179ce4ac2ed043c141f
rern <rernrern@gmail.com>
2017-08-04 17:09:03 +0700

    Update cmd.sh

--- _settings/cmd.sh
+++ _settings/cmd.sh
@@ -25,8 +25,10 @@ srestart() {
 }
 
 mountmmc() {
-	mkdir -p /tmp/p$1
-	mount /dev/mmcblk0p$1 /tmp/p$1
+	if [[ ! $( mount | grep p$1 ) ]]; then
+		mkdir -p /tmp/p$1
+		mount /dev/mmcblk0p$1 /tmp/p$1
+	fi
 }
 mountrune() {
 	mountmmc 9

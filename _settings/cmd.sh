8f92ceec0851680a8efd4bcf8154793c0d60a8aa
rern <rernrern@gmail.com>
2017-08-05 09:55:45 +0700

    Update cmd.sh

--- _settings/cmd.sh
+++ _settings/cmd.sh
@@ -36,14 +36,11 @@ sreload() {
 	systemctl start $1
 }
 
-mountmmc() {
+mmc() {
 	if [[ ! $( mount | grep p$1 ) ]]; then
 		mkdir -p /tmp/p$1
 		mount /dev/mmcblk0p$1 /tmp/p$1
 	fi
-}
-mountrune() {
-	mountmmc 9
 }
 
 bootosmc() {

db55e875dea7ecb9225d73b0ebbeddfbffce4d3a
rern <rernrern@gmail.com>
2017-08-05 15:15:03 +0700

    Update poweroff.py

--- OSMC_GPIO/home/osmc/poweroff.py
+++ OSMC_GPIO/home/osmc/poweroff.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python
-
-# need root access to /dev/mem, /sys/module
-# this script cannot run sudo by itself
 import gpiooff
 import os
-import sys
-	
-if len(sys.argv) == 1: # no argument
-	os.system('/usr/bin/xbmc-send -a "Powerdown"')
-else: # with any argument
-	os.system('/usr/bin/xbmc-send -a "Reboot"')
+
+os.system('/usr/bin/xbmc-send -a "Powerdown"')

48a4ff87c54d184bc2f925fd3926225f33e76b64
rern <rernrern@gmail.com>
2017-08-08 15:48:45 +0700

    Update install.sh

--- motd/install.sh
+++ motd/install.sh
@@ -2,6 +2,8 @@
 
 rm $0
 
+[[ -e /etc/motd.logo ]] && echo '"OSMC logo motd" already installed'; exit
+
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
 title -l = "$bar Install OSMC logo motd ..."

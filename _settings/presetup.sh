e34db948d513356560c166b7d92f5ed63eeb6ef8
rern <rernrern@gmail.com>
2017-08-08 21:13:20 +0700

    Update presetup.sh

--- _settings/presetup.sh
+++ _settings/presetup.sh
@@ -16,24 +16,21 @@ mmc() {
 
 echo -e "$bar Set HDMI mode ..."
 #################################################################################
-if ! grep -q '^hdmi_mode=' /boot/config.txt; then
-	mmc 1
-	if ! mount | grep -q 'mmcblk0p7'; then
-	  mmc 7
-	  mntroot=/tmp/p7
-	fi
-	# force hdmi mode, remove black border (overscan)
-	hdmimode='
+# force hdmi mode, remove black border (overscan)
+hdmimode='
 hdmi_group=1
 hdmi_mode=31      # 1080p 50Hz
 disable_overscan=1
-hdmi_ignore_cec=1 # disable cec
-'
-	! grep -q '^hdmi_mode=' /tmp/p1/config.txt && echo "$hdmimode" >> /tmp/p1/config.txt
-	! grep -q '^hdmi_mode=' /tmp/p7/boot/config.txt && echo "$hdmimode" >> $mntroot/boot/config.txt
-fi
-sed -i '/gpio/ s/^/#/
-' $mntroot/boot/config.txt
+hdmi_ignore_cec=1' # disable cec
+
+mmc 1
+mmc 6
+mmc 8
+! grep -q '^hdmi_mode=' /tmp/p1/config.txt && echo "$hdmimode" >> /tmp/p1/config.txt
+! grep -q '^hdmi_mode=' /tmp/p6/boot/config.txt && echo "$hdmimode" >> /tmp/p6/boot/config.txt
+! grep -q '^hdmi_mode=' /tmp/p8/boot/config.txt && echo "$hdmimode" >> /tmp/p8/boot/config.txt
+
+sed -i '/gpio/ s/^/#/' /tmp/p6/boot/config.txt
 echo
 
 echo -e "$bar Mount USB drive to /mnt/hdd ..."
@@ -42,15 +39,14 @@ mnt0=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
 label=${mnt0##/*/}
 mnt="/mnt/$label"
 mkdir -p "$mnt"
-fstabmnt="/dev/sda1  $mnt  ext4  defaults,noatime"
-if ! grep $mnt /etc/fstab &> /dev/null; then
-  echo "$fstabmnt" >> $mntroot/etc/fstab
-fi
+mmc 7
+mntroot=/tmp/p7
+echo "/dev/sda1  $mnt  ext4  defaults,noatime" >> $mntroot/etc/fstab
 echo
 
+echo -e "$bar Set apt cache ..."
+#################################################################################
 if [[ ! -L $mntroot/var/cache/apt ]]; then
-  echo -e "$bar Set apt cache ..."
-  #################################################################################
 	mkdir -p $mnt/varcache/apt
 	rm -r $mntroot/var/cache/apt
 	ln -s $mnt/varcache/apt $mntroot/var/cache/apt

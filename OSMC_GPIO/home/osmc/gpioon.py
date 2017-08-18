64099030f4e28daf90860c546c664eb72a8621ed
rern <rernrern@gmail.com>
2017-08-18 09:11:05 +0700

    Update gpioon.py

--- OSMC_GPIO/home/osmc/gpioon.py
+++ OSMC_GPIO/home/osmc/gpioon.py
@@ -1,27 +1,6 @@
 #!/usr/bin/python
-import RPi.GPIO as GPIO
-import json
+from gpio import *
 import os
-
-with open('/home/osmc/gpio.json') as jsonfile:
-	gpio = json.load(jsonfile)
-
-on = gpio['on']
-
-on1 = int(on['on1'])
-ond1 = int(on['ond1'])
-on2 = int(on['on2'])
-ond2 = int(on['ond2'])
-on3 = int(on['on3'])
-ond3 = int(on['ond3'])
-on4 = int(on['on4'])
-
-onx = [on1, on2, on3, on4]
-onx = [i for i in onx if i != 0]
-
-GPIO.setwarnings(0)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(onx, GPIO.OUT)
 
 if GPIO.input(onx[1]) == 0:
 	os.system('/usr/bin/xbmc-send -a "Notification(GPIO,Already ON)"')

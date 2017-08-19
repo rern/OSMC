a05388cde1b16acfd3b58e36c29a79fd119b68d1
rern <rernrern@gmail.com>
2017-08-19 11:19:48 +0700

    Update install.sh

--- OSMC_GPIO/install.sh
+++ OSMC_GPIO/install.sh
@@ -72,7 +72,7 @@ systemctl enable gpioset
 systemctl start gpioset
 
 # set permission #######################################
-sed 's/#includedir/includedir/' /etc/sudoers
+sed -i 's/#includedir/includedir/' /etc/sudoers
 echo 'osmc ALL=NOPASSWD: ALL' > /etc/sudoers.d/sudoers
 usermod -a -G root osmc # add user osmc to group root to allow /dev/gpiomem access
 #chmod g+rw /dev/gpiomem # allow group to access set in gpioset.py for every boot

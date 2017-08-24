bb6294fc1cd3cda1bb26630b5219bfa3410aaf59
rern <rernrern@gmail.com>
2017-08-24 17:54:23 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -13,7 +13,7 @@
 rm $0
 
 # import heading function
-wget -qN --show-progress --no-check-certificate https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
+wget -qN --show-progress https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 timestart l
 
 gitpath=https://github.com/rern/OSMC/raw/master

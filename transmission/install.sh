27fa6991f938d0628cd38313fa4c23ccea1aadc7
rern <rernrern@gmail.com>
2017-08-31 22:10:07 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -85,7 +85,7 @@ sed -i -e 's|"download-dir": ".*"|"download-dir": "'"$path"'"|
 ' $file
 
 # set password
-if [[ -n $pwd1 ]]; then
+if [[ -n $pwd1 && $pwd1 != 0 ]]; then
 	sed -i -e 's|"rpc-authentication-required": false|"rpc-authentication-required": true|
 	' -e 's|"rpc-password": ".*"|"rpc-password": "'"$pwd1"'"|
 	' -e 's|"rpc-username": ".*"|"rpc-username": "root"|

bc74b6c3e04588938d8370f2ce631acd0b53c1fc
rern <rernrern@gmail.com>
2017-09-13 07:57:03 +0700

    Update uninstall_motd.sh

--- motd/uninstall_motd.sh
+++ motd/uninstall_motd.sh
@@ -2,7 +2,7 @@
 
 wget -qN --show-progress https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
-if [[ ! -e /etc/motd.logo ]]; then
+if [[ ! -e /usr/local/bin/uninstall_motd.sh ]]; then
   echo -e "$info OSMC logo motd not found."
   exit
 fi

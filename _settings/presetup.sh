d61e613fe0ddfeda6caa07b49c00b05312aef352
rern <rernrern@gmail.com>
2018-01-08 18:02:27 +0700

    Update presetup.sh

--- _settings/presetup.sh
+++ _settings/presetup.sh
@@ -19,19 +19,19 @@ echo -e "$bar Set HDMI mode ..."
 #################################################################################
 # force hdmi mode
 if (( $# == 0 )); then
-	file=/boot/config.txt
+	mntroot=''
 else
 	mmc $1
-	file=/tmp/p$1/boot/config.txt
+	mntroot=/tmp/p$1
 	#sed -i '/gpio/ s/^/#/' $file
 fi
 
-if ! grep -q '^hdmi_mode=' $file; then
+if ! grep -q '^hdmi_mode=' $mntroot/boot/config.txt; then
 sed -i '$ a\
 hdmi_group=1\
 hdmi_mode=31\
 hdmi_ignore_cec=1	
-' $file
+' $mntroot/boot/config.txt
 fi
 echo
 
@@ -41,20 +41,10 @@ mnt0=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
 label=${mnt0##/*/}
 mnt="/mnt/$label"
 mkdir -p "$mnt"
-if (( $# == 0 )); then
-	file=/etc/fstab
-else
-	mmc $1
-	file=/tmp/p$1/etc/fstab
-fi
-echo "/dev/sda1  $mnt  ext4  defaults,noatime" >> $file
-echo
 
-echo -e "$bar Set apt cache ..."
-#################################################################################
-mkdir -p $mnt/varcache/apt
-rm -rf $mntroot/var/cache/apt
-ln -sf $mnt/varcache/apt $mntroot/var/cache/apt
+echo "/dev/sda1  $mnt  ext4  defaults,noatime" >> $mntroot/etc/fstab
+mount -a
+echo
 
 # disable setup marker files
 touch $mntroot/walkthrough_completed # initial setup
